eaa6c08d34e700ec6fb6a50503bdaf0ea741561b
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-14 18:00:07 +0530

    array and string

--- 2. Variables/index.ts
+++ 2. Variables/index.ts
@@ -13,12 +13,19 @@
 // console.log(anyData);  // (Object)
 
 
-let obj: {name: string, age: number, phone: any} = {
-    name: "Arbaz",
-    age: 22,
-    phone: undefined
-}
+// let obj: {name: string, age: number, phone: any} = {
+//     name: "Arbaz",
+//     age: 22,
+//     phone: undefined
+// }
 
-obj = {...obj, phone: 1000};
+// obj = {...obj, phone: 1000};
 
-console.log(obj)
+// console.log(obj)
+
+
+
+let myArr: (number | string)[] = [22, 3,33,421,"Arbaz"] 
+let myArr2: [number, string] = [22, "Arbaz"]
+console.log(myArr);
+console.log(myArr2);
