da04444e78b4d99c959743767420aa263b899a36
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-14 18:08:45 +0530

    creating userDefined keys object in TS

--- Prectice/index.ts
+++ Prectice/index.ts
@@ -1,10 +1,24 @@
-//Creating variables
-let a: number = 22;
-let b: string = "Arbaz";
-let c: boolean = true;
-let d: undefined = undefined;
-let e: null = null;
-let f: object = {
-    firstName: "Arbaz",
-    lastName: "Khan"
+//// Creating variables
+// let a: number = 22;
+// let b: string = "Arbaz";
+// let c: boolean = true;
+// let d: undefined = undefined;
+// let e: null = null;
+// let f: object = {
+//     firstName: "Arbaz",
+//     lastName: "Khan"
+// }
+
+
+//object
+let myObj: {} = {
+    name: "Arbaz Khan",
+    city: "New Delhi"
 }
+console.log(myObj);
+
+let definedObj: {name: string, phone: number} = {
+    name: "Arbaz Khan",
+    phone: 999
+}
+console.log(definedObj);
