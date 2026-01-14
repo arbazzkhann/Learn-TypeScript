e9647b0dbde48feb4a9f82e310dae187250cd062
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-14 17:57:56 +0530

    object in ts

--- 2. Variables/index.ts
+++ 2. Variables/index.ts
@@ -1,13 +1,24 @@
-// any
-// We can set any datatype (not a good prectice)
-let anyData: any = "hello";
-console.log(anyData);  // hello  (string)
+// // any
+// // We can set any datatype (not a good prectice)
+// let anyData: any = "hello";
+// console.log(anyData);  // hello  (string)
 
-anyData = 33;
-console.log(anyData);  // 33 (number)
+// anyData = 33;
+// console.log(anyData);  // 33 (number)
 
-anyData = {
-    firstName: "Arbaz",
-    lastName: "Khan"    
+// anyData = {
+//     firstName: "Arbaz",
+//     lastName: "Khan"    
+// }
+// console.log(anyData);  // (Object)
+
+
+let obj: {name: string, age: number, phone: any} = {
+    name: "Arbaz",
+    age: 22,
+    phone: undefined
 }
-console.log(anyData);  // (Object)
+
+obj = {...obj, phone: 1000};
+
+console.log(obj)
