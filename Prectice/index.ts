ca648dba1096e09f0602de8de17e364fa86e27c5
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-14 18:04:37 +0530

    creating JS core datatype variables in TS

---
+++ Prectice/index.ts
@@ -0,0 +1,10 @@
+//Creating variables
+let a: number = 22;
+let b: string = "Arbaz";
+let c: boolean = true;
+let d: undefined = undefined;
+let e: null = null;
+let f: object = {
+    firstName: "Arbaz",
+    lastName: "Khan"
+}
