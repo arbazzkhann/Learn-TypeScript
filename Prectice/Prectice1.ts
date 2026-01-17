f209037ef6c5c6b3ccc071add42431de08905ec3
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-17 17:41:12 +0530

    2nd prectice question - interface question

--- Prectice/Prectice1.ts
+++ Prectice/Prectice1.ts
@@ -6,3 +6,22 @@ function factorial(num: number): number {
     return num * factorial(num - 1)  //recursion
 }
 console.log(factorial(5));  //120
+
+
+/*
+    2. Define a function that accepts a user object (with - name, age, and email) and returns a formatted string.
+*/
+interface User {
+    name: string,
+    age: number,
+    email: string
+}
+
+function formattedObj(user: User): string {
+    return `Hello ${user.name}, your are ${user.age} years old and you are logged-in using ${user.email} address`;
+}
+console.log(formattedObj({
+    name: "Arbaz",
+    age: 22,
+    email: "arbaz@arbaz.com"
+}));
