ce847e50c8d36e7a2ab7c268cb932a4d696b4a00
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-20 13:33:12 +0530

    Access modifiers in TS

--- Prectice/classes.ts
+++ Prectice/classes.ts
@@ -1,19 +1,54 @@
-class Person {
-    name: string
-    age: number
+// class Person {
+//     name: string
+//     age: number
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age
+//     }
+
+//     printDetails(): void {
+//         console.log(`Your name is ${this.name} and you are ${this.age} years old.`);  
+//     }
+// }
+
+// const myName = new Person("Arbaz", 22);
+
+// myName.printDetails();
+// console.log(myName.name);
+// console.log(myName.age);
+
+
+// ────────────────────────────────────────── Access Modifiers
+// 1. public: Accessible from anywhere, This is the default if no modifier 
+// 2. private: Accessible within the class
+// 3. protected: Accessible within the class and its subclasses. 
+
+class Car1 {
+    public brand: string;
+    private model: string;
+    protected year: number;
+
+    constructor(brand: string, model: string, year: number) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
     }
 
-    printDetails(): void {
-        console.log(`Your name is ${this.name} and you are ${this.age} years old.`);  
+    //getter
+    getModel(): string {
+        return this.model
     }
 }
 
-const myName = new Person("Arbaz", 22);
+class Car2 extends Car1 {
+    year = 1990;
+}
 
-myName.printDetails();
-console.log(myName.name);
-console.log(myName.age);
+const myCar = new Car1("Mahindra", "XUV 700", 2026);
+
+console.log(myCar.brand);
+// console.log(myCar.model);   //gives error because private
+console.log("Model by getter: ", myCar.getModel());   //getting model indirectly
+
+// console.log(myCar.year);    //gives error because only can use with instance  //only use with inheritance
