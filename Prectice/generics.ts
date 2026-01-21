e6b2a383b241f5a00de5a8a99b36c3b7f047a292
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-21 19:47:04 +0530

    Generics definition and one example in TS

---
+++ Prectice/generics.ts
@@ -0,0 +1,15 @@
+// ────────────────────────────────────────── Generics
+// Generics in TypeScript are a tool for creating flexible, reusable components 
+// (functions, classes, interfaces, and types) that can work with a wide variety 
+// of data types while maintaining type safety.
+
+// Instead of writing a function that accepts only a number or only a string, 
+// you can write a generic function that accepts a type variable (commonly T, for "Type"), 
+// which acts as a placeholder for the actual type the user will specify or the compiler will infer. 
+
+function genericIdentity<T> (arg: T): T {
+    return arg;
+}
+
+console.log(genericIdentity<number>(22));
+console.log(genericIdentity<string>("Hello"));
