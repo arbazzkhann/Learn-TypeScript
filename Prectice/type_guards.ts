2bb010743e0127847fe7e6994a06761fe486c400
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-22 20:01:50 +0530

    definition of type guards in TS

---
+++ Prectice/type_guards.ts
@@ -0,0 +1,15 @@
+// ────────────────────────────────────────── Type guards
+/*
+Type guards: 
+    Expressions or functions that performs runtime checks to guarantee the type of a variable
+    within a specific scope.
+
+Without Type Guards:
+    TypeScript often cannot determine the specific type of a variable,
+    Leading to errors when accessing type-specific properties.
+
+Type Guards Provides:
+    - Enforcing Type Safety.
+    - Preventing Runtime Errors.
+    - Improving Code Intelligence.
+*/
