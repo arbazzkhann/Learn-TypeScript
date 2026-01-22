8787793237e4f5b6b3cd7bfe2dc0cf32fc830c54
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-22 16:24:46 +0530

    generics with interface

--- Prectice/generics.ts
+++ Prectice/generics.ts
@@ -1,15 +1,30 @@
-// ────────────────────────────────────────── Generics
-// Generics in TypeScript are a tool for creating flexible, reusable components 
-// (functions, classes, interfaces, and types) that can work with a wide variety 
-// of data types while maintaining type safety.
+// // ────────────────────────────────────────── Generics
+// // Generics in TypeScript are a tool for creating flexible, reusable components 
+// // (functions, classes, interfaces, and types) that can work with a wide variety 
+// // of data types while maintaining type safety.
 
-// Instead of writing a function that accepts only a number or only a string, 
-// you can write a generic function that accepts a type variable (commonly T, for "Type"), 
-// which acts as a placeholder for the actual type the user will specify or the compiler will infer. 
+// // Instead of writing a function that accepts only a number or only a string, 
+// // you can write a generic function that accepts a type variable (commonly T, for "Type"), 
+// // which acts as a placeholder for the actual type the user will specify or the compiler will infer. 
 
-function genericIdentity<T> (arg: T): T {
-    return arg;
+// function genericIdentity<T> (arg: T): T {
+//     return arg;
+// }
+
+// console.log(genericIdentity<number>(22));
+// console.log(genericIdentity<string>("Hello"));
+
+
+
+// ────────────────────────────────────────── Generics with Interface
+interface KeyPair <K, V> {
+    key: K;
+    value: V;
 }
 
-console.log(genericIdentity<number>(22));
-console.log(genericIdentity<string>("Hello"));
+const myObj: KeyPair <string, boolean> = {
+    key: "isLogin",
+    value: false
+}
+
+console.log(myObj);
