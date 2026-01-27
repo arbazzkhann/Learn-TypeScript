252dd3800faf8c3d9bb95f4bb678bb80e8969a68
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-27 14:00:04 +0530

    Index Signature Examples in TS

--- Prectice/index_signature.ts
+++ Prectice/index_signature.ts
@@ -1,5 +1,27 @@
 // Index signature:
 
-// It is a way to define types for objects with dynamic property names (keys) 
-// when the exact names are not known ahead of time, 
-// but the type of the keys and values follow a consistent pattern. 
+// It is a way to "define types for objects" with "dynamic property names" (keys) 
+// when the "exact names are not known", 
+// but the type of the keys and values follow a consistent pattern.
+
+
+// 1:
+interface StringArray {
+    [index: number]: string;
+}
+
+let myArr: StringArray = ["Arbaz", "Khan"];
+console.log(myArr[1]);
+
+
+// 2:
+interface Fruits {
+    [key: string]: string;
+}
+
+let myFruits: Fruits = {
+    Apple: "Red color",
+    Banana: "Yellow color",
+    Guava: "Green color"
+}
+console.log(myFruits["Banana"]);
