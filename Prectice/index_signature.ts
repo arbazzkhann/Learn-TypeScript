3b262384a01f7bd0d453657e8058621d0e793354
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-27 13:02:44 +0530

    Index Signature in TS definition

---
+++ Prectice/index_signature.ts
@@ -0,0 +1,5 @@
+// Index signature:
+
+// It is a way to define types for objects with dynamic property names (keys) 
+// when the exact names are not known ahead of time, 
+// but the type of the keys and values follow a consistent pattern. 
