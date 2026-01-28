b4656ba8f408367beeecf5e2ae6f04f8fb6dd290
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-28 13:36:34 +0530

    modules in TS

---
+++ Prectice/modules.ts
@@ -0,0 +1,18 @@
+//────────────────────────────────── Modules (import / export)
+/*
+Modules in TypeScript enable code organization, encapsulation, and reusability 
+by "splitting code into separate", self-contained files that execute in their own scope.
+
+Using ES6 "export" and "import" syntax, variables, functions, classes, and interfaces 
+can be shared between files. 
+
+Any file containing a top-level import or export is considered a module. 
+
+
+Named Exports: export const myVar = 1;
+Default Exports: export default MyClass;
+
+
+import { MyClass } from "./MyClass";
+import MyDefault from "./MyDefault";
+*/
